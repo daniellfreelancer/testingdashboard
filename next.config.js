eb52458cf7814857f4e0d6792a5cbb17a591537d
danieljlugo <daniel.jlugo@gmail.com>
2023-04-13 11:44:59 -0300

    update 2 next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,32 @@
 /** @type {import('next').NextConfig} */
 
 
-const widthPWA = require('next-pwa')
+// const widthPWA = require('next-pwa')
 
-const nextConfig = widthPWA({
-  reactStrictMode: true,
-  pwa:{
-    dest: "public",
+// const nextConfig = widthPWA({
+//   reactStrictMode: true,
+//   pwa:{
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   }
+// }) 
+
+// module.exports = nextConfig
+
+
+const withPWA = require('next-pwa');
+
+const pwaConfig = {
+  pwa: {
+    dest: 'public',
     register: true,
     skipWaiting: true,
-  }
-}) 
+  },
+};
 
-module.exports = nextConfig
+const nextConfig = {
+  // cualquier otra configuración de Next.js que desee establecer
+};
+
+module.exports = withPWA(Object.assign({}, nextConfig, pwaConfig));
