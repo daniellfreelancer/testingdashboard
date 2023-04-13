158fb14104ebb28ce185d5fdf0007df2a02f2354
danieljlugo <daniel.jlugo@gmail.com>
2023-04-13 11:47:33 -0300

    update 3 nextconfig

--- next.config.js
+++ next.config.js
@@ -15,18 +15,42 @@
 // module.exports = nextConfig
 
 
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
 
-const pwaConfig = {
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
-};
+// const pwaConfig = {
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//   },
+// };
+
+// const nextConfig = {
+//   // cualquier otra configuración de Next.js que desee establecer
+// };
+
+// module.exports = withPWA(Object.assign({}, nextConfig, pwaConfig));
+
+
+const withPWA = require('next-pwa');
 
 const nextConfig = {
   // cualquier otra configuración de Next.js que desee establecer
+  reactStrictMode: true,
 };
 
-module.exports = withPWA(Object.assign({}, nextConfig, pwaConfig));
+const pwaConfig = {
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    // otras opciones de configuración que desee establecer para la biblioteca next-pwa
+  },
+};
+
+module.exports = withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    swSrc: 'service-worker.js',
+    // otras opciones de configuración que desee establecer para el plugin GenerateSW
+  },
+});
