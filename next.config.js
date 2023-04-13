fc079029f91a7f223a79958eae7a9062b185c365
danieljlugo <daniel.jlugo@gmail.com>
2023-04-13 13:34:29 -0300

    default nextConfig

--- next.config.js
+++ next.config.js
@@ -2,23 +2,23 @@
 
 
 
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV === 'development',
 
-})
+// })
 
-module.exports = withPWA({
-  // next.js config
-    nextConfig : {
-    reactStrictMode: true,
-  }
-})
+// module.exports = withPWA({
+//   // next.js config
+//     nextConfig : {
+//     reactStrictMode: true,
+//   }
+// })
