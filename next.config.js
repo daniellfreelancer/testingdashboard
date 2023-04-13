0063cb7ac7720e0eeb85416bf02f84f3d95ac462
danieljlugo <daniel.jlugo@gmail.com>
2023-04-13 11:23:50 -0300

    create PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+
+const widthPWA = require('next-pwa')
+
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+module.exports = widthPWA({
+  reactStrictMode: true,
+  pwa:{
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development"
+  }
+})
